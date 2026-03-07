4ef762da1f3a1f48ca0692418103eadf949c9e98
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-03-07 10:41:46 +0100

    Group functions in functions.js file

--- functions.js
+++ functions.js
@@ -1,3 +1,8 @@
+export function addBookToLibrary(array, title, author, page, read) {
+  const newBook = new Book(title, author, page, read);
+  array.push(newBook);
+}
+
 export function addBookCard(title, author, page, read) {
   const li = document.createElement("li");
   li.classList.add("book-item");
@@ -9,4 +14,15 @@ export function addBookCard(title, author, page, read) {
   p.classList.add("book-content");
   p.innerText = `${title} by ${author}, ${page} pages, ${read}.`;
   return li;
+}
+
+export function Book(title, author, page, read) {
+  this.title = title;
+  this.author = author;
+  this.page = page;
+  this.read = read ? "read" : " not read yet";
+  this.id = crypto.randomUUID();
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.page} pages, ${this.read}.`;
+  };
 }
