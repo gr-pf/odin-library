b4da2567bebaf325b56bb2c2b4bcddf533282794
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-03-07 10:37:09 +0100

    Fix addBookCard function

--- functions.js
+++ functions.js
@@ -2,8 +2,10 @@ export function addBookCard(title, author, page, read) {
   const li = document.createElement("li");
   li.classList.add("book-item");
   const div = document.createElement("div");
+  li.appendChild(div);
   div.classList.add("book-card");
   const p = document.createElement("p");
+  div.appendChild(p);
   p.classList.add("book-content");
   p.innerText = `${title} by ${author}, ${page} pages, ${read}.`;
   return li;
