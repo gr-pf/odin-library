05b47d3c0bf9eb0491d06ae44e4ad7d3d84d1a59
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-03-07 13:31:41 +0100

    Finish main loop

--- script.js
+++ script.js
@@ -1,37 +1,78 @@
-const myLibrary = [];
+import {
+  addBookToLibrary,
+  addBookCard,
+  Book,
+  removeBookById,
+  removeLiBookById,
+} from "./functions.js";
+
+let myLibrary = [];
 const myBooks = document.querySelector(".list-books");
 
-function Book(title, author, page, read) {
-  this.title = title;
-  this.author = author;
-  this.page = page;
-  this.read = read ? "read" : " not read yet";
-  this.id = crypto.randomUUID();
-  this.info = function () {
-    return `${this.title} by ${this.author}, ${this.page} pages, ${this.read}.`;
-  };
+const initBook1 = new Book("the hobbit", "tolkien", 152, true);
+addBookToLibrary(
+  myLibrary,
+  initBook1.title,
+  initBook1.author,
+  initBook1.page,
+  initBook1.read,
+);
+myBooks.appendChild(
+  addBookCard(
+    initBook1.title,
+    initBook1.author,
+    initBook1.page,
+    initBook1.read,
+    initBook1.id,
+  ),
+);
+const initBook2 = new Book("La Distinction", "Bourdieu", 563, false);
+addBookToLibrary(
+  myLibrary,
+  initBook2.title,
+  initBook2.author,
+  initBook2.page,
+  initBook2.read,
+);
+myBooks.appendChild(
+  addBookCard(
+    initBook2.title,
+    initBook2.author,
+    initBook2.page,
+    initBook2.read,
+    initBook2.id,
+  ),
+);
+
+function activeDelBtn() {
+  const delButtons = document.querySelectorAll(".book-del-btn");
+  delButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      e.preventDefault();
+      const bookId = e.target.getAttribute("data-id");
+      myLibrary = removeBookById(myLibrary, bookId);
+      removeLiBookById(e);
+    });
+  });
 }
+activeDelBtn();
 
-function addBookToLibrary(title, author, page, read) {
-  const newBook = new Book(title, author, page, read);
-  myLibrary.push(newBook);
-}
-
-addBookToLibrary("the hobbit", "tolkien", 152, true);
-addBookToLibrary("La Distinction", "Bourdieu", 563, false);
-
-function showBooks() {
-  for (let book of myLibrary) {
-    console.log(book.title);
-  }
-}
-
-showBooks();
-//console.log(myLibrary[0].title);
-
-const list = document.createElement("li");
-const para = document.createElement("p");
-
-myBooks.appendChild(list);
-list.appendChild(para);
-para.innerText = myLibrary[0].title;
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("#form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const obj = Object.fromEntries(formData);
+    const newBook = new Book(obj.title, obj.author, obj.pages, obj.read);
+    myBooks.appendChild(
+      addBookCard(
+        newBook.title,
+        newBook.author,
+        newBook.page,
+        newBook.read,
+        newBook.id,
+      ),
+    );
+    activeDelBtn();
+  });
+});
